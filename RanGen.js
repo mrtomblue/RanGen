27f54d56695704864601e94e05ef93bee20bc70d
MR.TOMBLUE <81450661+mrtomblue@users.noreply.github.com>
2022-12-03 18:42:56 -0500

    add a couple comments for todo

--- RanGen.js
+++ RanGen.js
@@ -1,5 +1,11 @@
-// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const symbols = ["@","#","*","%","$","!","-","_"];
+// TODO make look nicer
+// TODO add comments to explain code
+// TODO add snackbar/walk through
+// TODO add custom symbols option
+// ? make error function throw error using "new ERROR()" ?
+// ? change primary layout to grid instead of flex layout
+// ! fix error bug where the error disappear when you change input but there is still invalid input
+
 const lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
